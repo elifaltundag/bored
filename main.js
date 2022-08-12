c9299ecc33f55446e185e3989c6b7e666f5d5af7
elifaltundag <elif.altundag.x@gmail.com>
2022-08-12 21:12:13 +0300

    Upload first draft files

---
+++ main.js
@@ -0,0 +1,19 @@
+const activityElm = document.getElementById("activity");
+
+function getRandomActivity() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(resp => resp.json())
+    .then(data => activityElm.textContent = "How about you " +  data.activity.toLowerCase() + "?");
+    
+  changeGradDeg();
+}
+
+function changeGradDeg() {
+  const currentGradDeg = getComputedStyle(document.documentElement).getPropertyValue("--deg");
+  const currentDeg = parseInt(currentGradDeg.slice(0, currentGradDeg.indexOf("deg")));
+  
+  const newDeg = currentDeg + 30;
+  const newGradDeg = newDeg.toString() + "deg"
+  
+  document.documentElement.style.setProperty("--deg", newGradDeg);
+}
